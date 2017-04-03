6bc3add572acf37f036b6c703479fe8addeb6636
Hamza Hajji <hmzhjji@gmail.com>
2017-04-03 20:17:11 +0100

    04_pig_latin solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
 #write your code here
+def pig_latin(word)
+	if "aeiou".index(word[0]) != nil # starts with vowel
+		word + "ay"
+	else # starts with consonant
+		first_cons = ""
+		i = 0
+		while "aeiou".index(word[i]) == nil
+			if word[i] == "q" and word[i + 1] == "u"
+				first_cons += "qu"
+				break
+			end
+			first_cons += word[i]
+			i += 1
+		end
+		word[first_cons] = ""
+		word + first_cons + "ay"
+	end
+end
+
+def translate(sentence)
+	pig_latined = ""
+	sentence.split(" ").each do |word|
+		pig_latined += pig_latin(word) + " "
+	end
+	pig_latined.chop
+end
