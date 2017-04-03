60a785f540271cbfdcdfa87527585bdc08aaa841
Hamza Hajji <hmzhjji@gmail.com>
2017-04-03 20:33:44 +0100

    05_book_titles solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+	# write your code here
+	attr_accessor :title
+
+	def title()
+		titleized = ""
+		(@title.split(" ")).each do |word|
+			if ["a", "an", "but", "or", "for", "nor", "and", "in", "of", "on", "over", "the"].index(word) == nil
+				titleized += word.capitalize + " "
+			else
+				titleized += word + " "
+			end
+		end
+		titleized[0] = titleized[0].upcase
+		titleized.chop
+	end
 end
